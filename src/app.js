b7a91434ae26da35126d680a749e4319b7e8e204
kaisarsofi-kxar <kaisersofi.ks2515@gmail.com>
2023-11-21 20:06:08 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -8,4 +8,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRoutes from "./routes/user.routes.js";
+
+//routes decleration
+app.use("/api/v1/users", userRoutes);
 export { app };
